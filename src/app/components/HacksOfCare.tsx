ece0b366c103f61154298f6dc4a4fb1486105b5f
noiramschneider <schneidermarionx@gmail.com>
2025-02-12 13:40:09 -0500

    changed spacing with logo

--- src/app/components/HacksOfCare.tsx
+++ src/app/components/HacksOfCare.tsx
@@ -161,7 +161,7 @@ export default function HacksOfCare() {
   if (!isClient) return null;
 
   const isMobile = columns === 1;
-  const gridTop = isMobile ? "10%" : "30%"; // Espace entre le logo et la grille
+  const gridTop = isMobile ? "20%" : "30%"; // Espace entre le logo et la grille
   const gridGap = isMobile ? "0.5rem" : "1rem"; // Espacement entre les hacks
   const gridHeight = isMobile ? "80%" : "65%";
 
